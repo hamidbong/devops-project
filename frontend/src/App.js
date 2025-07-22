f17c97fbc6818fb19052fc07efd0793e39f50931
hamidbong <117206678+hamidbong@users.noreply.github.com>
2025-07-22 21:14:29 +0100

    update frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+      <h1>🚀 Frontend Deployé sur Kubernetes !</h1>
+      <p>Ceci est une application React minimaliste.</p>
+    </div>
+  );
+}
+
+export default App;
