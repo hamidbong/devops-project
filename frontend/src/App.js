3c9b4153aff54ce98a458f02bf5c32156a1b4582
hamidbong <117206678+hamidbong@users.noreply.github.com>
2025-08-20 09:08:06 +0100

    nes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import Login from './pages/Login/Login';
 import Products from './pages/Products/Products';
 import Register from './pages/Register/Register';
+import Home from './pages/Home';
 import logo from './logo.svg';
 import Header from './pages/Header/header'; // Adaptez le chemin si besoin
 import { Routes, Route } from 'react-router-dom';
@@ -11,6 +12,7 @@ function App() {
     <>
       <Header></Header>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login></Login>}></Route>
         <Route path="/Dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="/Products" element={<Products />} />
