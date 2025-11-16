c6582f063ed0da29f47dfc46bee27cabfabc4b71
hamidbong <brahbong@gmail.com>
2025-11-16 18:30:42 +0000

    inventory update and terraform state

--- infra/terraform-devstack/main.tf
+++ infra/terraform-devstack/main.tf
@@ -121,6 +121,25 @@ resource "openstack_networking_secgroup_v2" "k8s_master_sg" {
 }
 
 # API Server
+resource "openstack_networking_secgroup_rule_v2" "master_ssh_rules" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.k8s_master_sg.id
+}
+resource "openstack_networking_secgroup_rule_v2" "master_icmp_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.k8s_master_sg.id
+}
+
+
+
 resource "openstack_networking_secgroup_rule_v2" "master_api" {
   security_group_id = openstack_networking_secgroup_v2.k8s_master_sg.id
   direction         = "ingress"
